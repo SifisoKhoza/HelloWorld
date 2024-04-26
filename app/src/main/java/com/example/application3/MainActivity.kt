7c86de582eb631ce0763a17ee3f6a1dba8ea332d
sifisosendoda <sifisosendodak@gmail.com>
2024-04-26 17:55:43 +0200

    Commit 2

--- app/src/main/java/com/example/application3/MainActivity.kt
+++ app/src/main/java/com/example/application3/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.application3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textDisp = findViewById<TextView>(R.id.textDisp)
+        val MozBtn: ImageButton = findViewById(R.id.MozBtn)
+        val FrenchBtn: ImageButton = findViewById(R.id.FrenchBtn)
+        val saButton: ImageButton = findViewById(R.id.saButton)
+        val NetherlandsBtn: ImageButton = findViewById(R.id.NetherlandsBtn)
+
+        MozBtn.setOnClickListener { textDisp.text = "Ola !" }
+        FrenchBtn.setOnClickListener { textDisp.text = "Bonjour!" }
+        saButton.setOnClickListener { textDisp.text = "Dumela !" }
+        NetherlandsBtn.setOnClickListener { textDisp.text = "Groet !" }
+
+
     }
 }
